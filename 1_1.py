ac3c4203f1aec0cc0a74079c7146b6271c630282
myazi <869233039@qq.com>
2017-11-22 15:26:28 +0800

    first commit

---
+++ 1_1.py
@@ -0,0 +1,471 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 11 21:33:19 2017
+
+@author: myazi
+"""
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+
+def load_dataset():
+    """
+    加载数据，并完成对数据的预处理，由于每一张图片表示形式为64*64*3，
+    这里需要将每张图片拉成列向量，多张图片一起构成一个大矩阵
+    """
+    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
+    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+
+    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
+    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+
+    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
+    
+    train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
+    
+    """
+    test=([[[[1,2,3],[4,5,6]],
+            [[7,8,9],[10,11,12]],
+            [[13,14,15],[16,17,18]]],
+            [[[19,20,21],[22,23,24]],
+            [[25,26,27],[28,29,30]],
+            [[31,32,33],[34,35,36]]]
+    ])
+    test=np.array(test)
+    print(test.shape)
+    test1=test.reshape((test.shape[1]*test.shape[2]*test.shape[3],test.shape[0]))
+    
+    print(test1)
+    
+    test2=test.reshape((test.shape[0],-1)).T
+    
+    print(test2)
+    
+    print(test)
+    
+    """
+    
+    """
+    完成图片拉成列向量
+    /255防止在迭代过程中出现nan，因为激活函数收敛速度惊人
+    """
+    train_set_x_flatten = train_set_x_orig.reshape(train_set_x_orig.shape[0], -1).T/255.0 
+    test_set_x_flatten = test_set_x_orig.reshape(test_set_x_orig.shape[0], -1).T/255.
+    
+    return train_set_x_flatten, train_set_y_orig, test_set_x_flatten, test_set_y_orig, classes
+
+"""
+范数
+
+"""
+def L1(yhat,y):
+    m=y.shape[1]
+    loss=sum(np.abs(yhat-y))
+    return loss/m
+
+def L2(yhat,y):
+    m=y.shape[1]
+    loss=np.dot((y-yhat),(y-yhat).T)
+    return loss/m
+ 
+def init_parameters(in_n,hid_n,out_n):
+    
+    """
+    一个简单的一个隐藏层的神经网络初始化
+    """ 
+    
+    np.random.seed(1)
+    W1 = np.random.randn(hid_n, in_n)*0.01
+    b1 = np.zeros((hid_n, 1))
+    W2 = np.random.randn(out_n, hid_n)*0.001
+    b2 = np.zeros((out_n, 1))
+    assert(W1.shape==(hid_n, in_n))
+    assert(b1.shape==(hid_n, 1))
+    assert(W2.shape==(out_n, hid_n))
+    assert(b2.shape==(out_n, 1))
+    
+    parameters = {"W1": W1,
+                  "b1": b1,
+                  "W2": W2,
+                  "b2": b2}
+    
+    return parameters
+ 
+def init_parameters_deep(layer_dims):
+    
+    """
+    可调整超参的神经网络初始化
+    网络层数
+    每层神经元个数
+    当然每一层使用的激活函数也可以放到这里来设置
+    """ 
+    
+    np.random.seed(1)
+    parameters = {}
+    L = len(layer_dims)            # number of layers in the network
+
+    for l in range(1, L):
+        parameters['W' + str(l)] = np.random.randn(layer_dims[l], layer_dims[l-1]) / np.sqrt(layer_dims[l-1]) #*0.01
+        parameters['b' + str(l)] = np.zeros((layer_dims[l], 1))
+        
+        assert(parameters['W' + str(l)].shape == (layer_dims[l], layer_dims[l-1]))
+        assert(parameters['b' + str(l)].shape == (layer_dims[l], 1))
+
+    return parameters
+
+def line_forword(A,W,b):
+    
+    """
+    前向传导
+    线性函数计算
+    其中A是上一层神经元的激活值
+    并将当前层W，b，A保存下来是为了在反向传播中需要使用到
+    """
+    
+    Z=W.dot(A)+b
+    
+    assert(Z.shape==(W.shape[0],A.shape[1]))
+    cache=(A,W,b)
+    
+    return Z,cache
+
+def sigmoid(Z):
+    
+    """
+    前向传导
+    sigmoid函数计算，且是矩阵计算，
+    保存Z是因为在反向传播中需要计算dA，
+    为了代码简洁而保存Z,完全可以只保存A,W,b
+    """
+    
+    A=1/(1+np.exp(-Z))
+    
+    assert(A.shape==Z.shape)
+    cache=Z
+    return A,cache
+
+def relu(Z):
+    
+    """
+    前向传导
+    relu修正单元函数计算，矩阵计算，
+    """
+    
+    A=np.maximum(0,Z)
+    
+    
+    assert(A.shape == Z.shape)
+    cache = Z 
+    
+    return A, cache
+
+def line_active_forword(Apre,W,b,activation):
+    
+    """
+    前向传导
+    线性函数，激活函数计算
+    并且保存线性函数左边的值，与激活函数左边的值用于反向传播
+    """
+    
+    Z,line_cache=line_forword(Apre,W,b)
+    
+   # print("===================")
+    #print(Z.shape)
+    
+    if(activation=="sigmoid"):
+        A,active_cache=sigmoid(Z)
+        
+    if(activation=="rule"):
+        A,active_cache=relu(Z)
+        
+    assert(A.shape==(W.shape[0],Apre.shape[1]))
+    
+    cache=(line_cache,active_cache)
+    
+    return A,cache
+
+def modle_forword(X,parameters):
+    
+    """
+    前向传导
+    将输入特征给A0，根据参数，线性函数，激活函数计算
+    计算过程中保存每一层中的函数左边的值
+    即A,W,b,Z
+    """
+    
+    caches=[];
+    A=X
+    L=len(parameters)//2
+    
+    for l in range(1,L):
+        Apre=A
+        A , cache = line_active_forword(Apre,parameters["W" + str(l)],parameters["b" + str(l)],activation="rule")
+        caches.append(cache)
+        
+    AL , cache = line_active_forword(A,parameters["W" + str(L)],parameters["b" + str(L)],activation="sigmoid")
+    caches.append(cache)
+    
+    assert(AL.shape==(1,X.shape[1]))
+    
+    return AL,caches
+
+def relu_backword(dA,cache):
+    
+    """
+    relu函数的导数0,1
+    而导数值取决于Z是否大于0
+    """
+    
+    Z=cache
+    
+    dZ = np.array(dA, copy=True)
+    
+    dZ[Z <= 0] = 0
+
+    
+    assert (dZ.shape == Z.shape)
+    
+    return dZ
+
+def sigmoid_backword(dA,cache):
+    
+    """
+    反向传播
+    sigmoid函数的导数是s（1-s）
+    而损失函数对Z的偏导为dAs（1-s）
+    这里，如果保存了当前层的激活值，完全可以不用再计算一遍s
+    但是当前层cache中保存的是A,W,b,Z,其中有Z=wA+b，不是A=g（z）,
+    所以之前sigmoid中保存了z，且在反向传播中再计算一遍A
+    """
+    Z=cache
+    
+    s=1/(1+np.exp(-Z))
+    
+    dZ=dA*s*(1-s)
+
+    
+    assert (dZ.shape == Z.shape)
+    
+    return dZ
+
+def line_backward(dZ,cache):
+    
+    """
+    反向传播
+    线性函数求导同时乘上链式求导下来的dZ
+    dA=W.T*dZ
+    dW=1.0/m*dZ*A
+    db=1.0/m*dZ
+    """
+    
+    Apre, W, b = cache
+    
+    m = Apre.shape[1]
+
+    dW=1.0 / m * np.dot(dZ,Apre.T)
+    db = 1.0 / m * np.sum(dZ, axis = 1, keepdims = True)
+    
+    dApre=np.dot(W.T,dZ)
+
+    
+    assert (dApre.shape == Apre.shape)
+    assert (dW.shape == W.shape)
+    assert (db.shape == b.shape)
+    
+    return dApre,dW,db
+
+def line_active_backword(dA,cache,activation):
+    
+    """
+    反向传播
+    激活函数，线性函数求导
+    """
+    
+    line_cache,activation_cache=cache
+    
+    if(activation=="relu"):
+        
+        dZ=relu_backword(dA,activation_cache)
+        dApre,dW,db=line_backward(dZ,line_cache)
+        
+    if(activation=="sigmoid"):
+        
+        dZ=sigmoid_backword(dA,activation_cache)
+        dApre,dW,db=line_backward(dZ,line_cache)
+        
+    return dApre,dW,db
+
+def modle_backward(AL,Y,caches):
+        
+    """
+    反向传播
+    1损失函数对AL的导数单独计算
+    2重复计算激活函数，线性函数的导数，
+    但值得注意的是，实际上还是损失函数对参数求偏导
+    保存每一层参数的导数用于更新参数
+    """
+    
+    grads={}
+    L=len(caches)
+
+    Y = Y.reshape(AL.shape)
+    
+    dAL= - (np.divide(Y, AL) - np.divide((1 - Y ), (1 - AL)))#dF/dAL视损失函数而定，对数线性模型损失，最小均方差损失    
+    
+    current_cache=caches[L-1]
+    
+    grads["dA" + str(L)], grads["dW" + str(L)], grads["db" + str(L)]=line_active_backword(dAL, current_cache, activation = "sigmoid")
+    
+    for l in reversed(range(L-1)):#l L-2,L-3,...L=0
+    
+        current_cache=caches[l]
+        
+        dA_pre, dW, db=line_active_backword(grads["dA" + str(l+2)],current_cache,"relu")
+        
+        grads["dA" + str(l+1)]=dA_pre
+        grads["dW" + str(l+1)]=dW
+        grads["db" + str(l+1)]=db
+        
+    return grads
+        
+def cumpute_loss(AL,Y):
+    
+    """
+    计算损失值
+    """
+    
+    m = Y.shape[1]
+    
+    cost = (1.0/m) * (-np.dot(Y,np.log(AL).T) - np.dot(1-Y, np.log(1-AL).T))
+    
+    
+    cost = np.squeeze(cost)      # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
+        
+    assert(cost.shape == ())
+    
+    return cost
+
+def update_parameters(parameters,grads,learn_rate):
+    
+    """
+    上一次参数值，梯度，学习速率
+    更新参数
+    """
+    
+    L=len(parameters)//2
+    
+    for l in range(L):
+        parameters["W" + str(l+1)]-=learn_rate*grads["dW" + str(l+1)]
+        parameters["b" + str(l+1)]-=learn_rate*grads["db" + str(l+1)]
+           
+    return parameters
+
+def two_layer_modle(X,Y,layer_dims,learn_rate=0.01,num_iterations = 3000, print_cost=True):
+    
+    """
+    神经网络模型
+    输入特征，输出特征，网络结构(可以包括每一层的激活函数类型)，学习速率，迭代次数，损失值
+    """
+    
+    grads = {}
+    costs = []
+
+    parameters=init_parameters_deep(layer_dims)
+    
+    i=0
+    while i<num_iterations:
+        AL,caches=modle_forword(X,parameters)
+        
+        cost=cumpute_loss(AL,Y)
+        
+        grads=modle_backward(AL,Y,caches)
+    
+        parameters=update_parameters(parameters,grads,learn_rate)
+        
+        if print_cost and i%10==0:
+            print(cost)
+            costs.append(cost)
+        i+=1
+    plt.plot(np.squeeze(costs))
+    plt.ylabel('cost')
+    plt.xlabel('iterations (per tens)')
+    plt.title("Learning rate =" + str(learn_rate))
+    plt.show()
+    
+    return parameters
+
+def predict(X,Y,parameters):
+    
+    """
+    预测
+    测试集X,Y,训练参数
+
+    """
+    m=X.shape[1]
+    
+    pro=np.zeros((1,m))
+    
+    A,cache=modle_forword(X,parameters)
+    print(A.shape)
+    
+    for i in range(0, A.shape[1]):
+        if(A[0,i]>0.5):
+            pro[0,i]=1
+        else:
+            pro[0,i]=0
+    print(pro)
+    print(Y)
+    acc=0
+    for i in range(0,A.shape[1]):
+        if(pro[0,i]==Y[0,i]):
+            acc+=1
+    print(acc)    
+    print("Accuracy: "  + str(np.sum((pro - Y ==0.0)/m)))
+        
+    return pro
+  
+def main():
+        
+    """
+    主函数
+    1加载训练集，预处理
+    2初始化网络结构，超参，参数
+    3训练
+        1前向传导
+        2计算损失值
+        3反向传播
+        4更新参数
+    4预测
+
+    """  
+    train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes=load_dataset()
+    
+    
+    train=np.concatenate([train_set_x_orig, train_set_y_orig],axis=0)
+    np.savetxt("text.txt",train)
+    
+    input(q)
+    
+    in_n=train_set_x_orig.shape[0]
+    hid_n=7
+    out_n=1
+    layer_dims=(in_n,hid_n,out_n)
+    
+    layer_dims=(12288,20,7,5,1)
+    parameters=two_layer_modle(train_set_x_orig,train_set_y_orig,layer_dims)
+    predict(test_set_x_orig,test_set_y_orig,parameters)
+    print(test_set_y_orig)
+    
+    
+    
+    
+    
+
+    
+    
+
+    
+    
